f1e8ed4050f541dfdfc9ba30fbe4901e7f2afd55
David Eastmond <davebuck1981@gmail.com>
2022-11-24 10:12:06 -0500

    add headlines and topics option to user settings

--- src/scenes/settings/Settings.tsx
+++ src/scenes/settings/Settings.tsx
@@ -1,5 +1,6 @@
 import { Box, Tab, Tabs, Typography } from "@mui/material";
 import { useState } from "react";
+import { HeadlinesTopicsPanel } from "../../components/headlines-topics-panel";
 import { NewsSourcesPanel } from "../../components/news-sources-panel";
 import { PasswordPanel } from "../../components/password-panel";
 
@@ -49,6 +50,7 @@ function Settings(props: ISettingsProps) {
         <Tabs value={value} onChange={handleChange} aria-label="hub tabs">
           <Tab label="Password & Profile" {...a11yProps(0)} />
           <Tab label="News Sources" {...a11yProps(1)} />
+          <Tab label="Headlines & Topics" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
@@ -56,6 +58,9 @@ function Settings(props: ISettingsProps) {
       </TabPanel>
       <TabPanel value={value} index={1}>
         <NewsSourcesPanel hasSession={props.hasSession} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <HeadlinesTopicsPanel hasSession={props.hasSession} />
       </TabPanel>
     </Box>
   );
